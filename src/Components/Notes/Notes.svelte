2814debef7d0e165ced2c42136807b5c579a24c8
lanxeon <bswapnonil@yahoo.com>
2021-10-17 12:08:40 +0530

    improved store subscription and unsub

--- src/Components/Notes/Notes.svelte
+++ src/Components/Notes/Notes.svelte
@@ -1,20 +1,12 @@
 <script lang="ts">
   // components
   import Card from "../Common/Card.svelte";
-  // models/interfaces
-  import type { Note } from "../../Core/models/Note";
   // store
   import NotesListStore from "../../Stores/NotesList";
-
-  let notes: Note[] = [];
-
-  NotesListStore.subscribe((data: Note[]) => {
-    notes = data;
-  });
 </script>
 
 <section class="cards">
-  {#each notes as note (note.id)}
+  {#each $NotesListStore as note (note.id)}
     <Card headerText={note.title} content={note.body} />
   {/each}
 </section>
