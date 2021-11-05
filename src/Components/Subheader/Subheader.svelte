ccbea009888be78d0cb8a67c69434814da8aad1e
lanxeon <bswapnonil@yahoo.com>
2021-11-06 00:53:47 +0530

    created subheader and subheader buttons reusable component (struggling with unused css selector issue)

---
+++ src/Components/Subheader/Subheader.svelte
@@ -0,0 +1,38 @@
+<script>
+  const { default: SubheaderButton } = require("./SubheaderButton.svelte");
+</script>
+
+<div class="subheader">
+  <div class="subheader__content">
+    <div class="subheader__content__left">
+      <SubheaderButton text="All" color="blue" active />
+      <SubheaderButton text="Home" color="orange" active />
+      <SubheaderButton text="Personal" color="purple" active />
+      <SubheaderButton text="Work" color="green" active />
+    </div>
+    <div class="subheader__content__right">
+      <button> + ADD NOTE </button>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .subheader {
+    width: 100%;
+    max-width: 840px;
+    padding: 9px;
+    margin: 0.5rem auto;
+
+    &__content {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+
+      &__left {
+        flex-direction: row;
+      }
+    }
+  }
+</style>
---
+++ src/Components/Subheader/SubheaderButton.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  export let color: "orange" | "blue" | "green" | "purple" = "blue";
+  export let text: string = "";
+  export let showDot: boolean = false;
+  export let active: boolean = false;
+</script>
+
+<button class={color === "orange" + active ? " active" : ""}>
+  <span>{text}</span>
+
+  {#if showDot}
+    <span class="dot">•</span>
+  {/if}
+</button>
+
+<style lang="scss">
+  button {
+    width: 80px;
+    color: #00000099;
+    opacity: 80%;
+
+    &.orange {
+      .dot {
+        color: #ff9100;
+      }
+
+      &.active {
+        background-color: #ff9100;
+        color: #fff;
+      }
+    }
+
+    &.blue {
+      .dot {
+        color: #69bcff;
+      }
+
+      &.active {
+        background-color: #69bcff;
+        color: #fff;
+      }
+    }
+
+    &.purple {
+      .dot {
+        color: #5c6bc0;
+      }
+
+      &.active {
+        background-color: #5c6bc0;
+        color: #fff;
+      }
+    }
+
+    &.green {
+      .dot {
+        color: #66bb6a;
+      }
+
+      &.active {
+        background-color: #66bb6a;
+        color: #fff;
+      }
+    }
+  }
+</style>
